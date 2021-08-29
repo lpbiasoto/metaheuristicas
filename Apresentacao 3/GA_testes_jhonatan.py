9f56ccb5fdcc38e0fb0fbb8d4073bbb22c4770bd
albertini-j <66831089+albertini-j@users.noreply.github.com>
2021-08-28 22:56:45 -0300

    GA teste e inclusão de funções no Funções GA

---
+++ Apresentacao 3/GA_testes_jhonatan.py
@@ -0,0 +1,156 @@
+from pathlib import Path
+import os
+import numpy as np
+import pandas as pd
+import time
+from numba import jit
+import pickle
+from funcoes_BuscaLocal import *
+from funcoes_GA import *
+from funcoes_gerais import *
+
+dados = {}
+
+with open("dados.pkl", "rb") as infile:
+    dados = pickle.load(infile)
+
+solucoes_pandas = pd.read_pickle('solucoes.pkl')
+solucoes = solucoes_pandas.to_dict()
+solucoes_GA = {}
+
+lista_hs = [0.8, 0.6, 0.4, 0.2]
+#conjuntos = [10,20,50,100,200,500,1000]
+conjuntos = [1000]
+lista_z = [0.25 , 0.5 , 0.6 , 0.75, 2]
+#lista_problemas = list(range(1,11))
+lista_problemas = [5]
+
+populacao = {}
+qtd_pop_inicial = {}
+for conjunto in conjuntos:
+    for h in lista_hs:
+        for problema in lista_problemas:
+            inicio=time.time()
+            pi = np.array(dados[conjunto][problema]['pi'])
+            ai = np.array(dados[conjunto][problema]['ai'])
+            bi = np.array(dados[conjunto][problema]['bi'])
+
+            d=int(sum(pi)*h)
+
+            lista_populacao_inicial = np.array([solucoes[(c,p,hi,z_corte)] for (c,p,hi,z_corte) in solucoes if conjunto == c and problema == p and h == hi])
+
+            melhor_obj, melhor_sol = 99999999999, np.array([])
+            obj_original = 0
+            for individuo in lista_populacao_inicial:
+                obj = calcula_objetivo(individuo, ai, bi, pi, d)
+                if obj < melhor_obj:
+                    melhor_obj, melhor_sol = obj, individuo
+                    obj_original = melhor_obj
+
+
+
+            populacao[(conjunto, h, problema)] = lista_populacao_inicial
+
+            n_iter_ga = 3000
+            taxa_mutacao_inicial = 0
+            n_pop_inicial = max(50,conjunto*0.1)
+            perc_pais_pop = 0.2
+            num_pais_duelo = 2
+
+            populacao_total = lista_populacao_inicial
+            populacao_pais = lista_populacao_inicial
+
+            for iter in range(n_iter_ga):
+                # print('##1')
+                ini = time.time()
+                populacao_filhos = []
+                taxa_mutacao = taxa_mutacao_inicial
+                #taxa_mutacao = (n_iter_ga - iter)/n_iter_ga*(taxa_mutacao_inicial-0.01) + 0.01
+                # print('##2')
+
+                if iter <= n_iter_ga/2:
+                    n_pop = int(n_pop_inicial + iter/2)
+                else:
+                    n_pop = int(n_pop_inicial + n_iter_ga/2 - iter/2)
+                # print('##3')
+
+                for i in range(0, (n_pop*2 - len(populacao_total))):
+                    pai1 = coliseu(populacao_pais,num_pais_duelo, ai, bi, pi, d)[0]
+                    pai2 = coliseu(populacao_pais,num_pais_duelo, ai, bi, pi, d)[0]
+                    filho = crossover_r(pai1, pai2)
+                    filho_mutacao = mutacao(filho, taxa_mutacao)
+                    populacao_filhos.append(filho_mutacao)
+                # print('##4')
+                
+                populacao_total = np.vstack((populacao_total,populacao_filhos))
+                
+                # print('##5')
+                cromossomos_unicos = list(map(np.array, set(map(tuple, populacao_total))))
+                
+                # print('##6')
+                fitness_cromossomos = []
+                cromossomos_reparados = []
+
+                for cromossomo in cromossomos_unicos:
+                    # breakpoint()
+                    # print('##6.1', cromossomo)
+                    solucao, obj = calcula_objetivo_GA(cromossomo, ai, bi, pi, d)
+                    
+                    # print('##6.2', cromossomo)
+                    fitness_cromossomos.append(obj)
+                    cromossomos_reparados.append(solucao)
+                
+                # print('##7')
+                fitness_cromossomos = np.array(fitness_cromossomos)
+                cromossomos_reparados = np.array(cromossomos_reparados)
+                
+                sobreviventes = oprime_fracos(fitness_cromossomos, n_pop)
+                # print('##8')
+
+                cromossomos_unicos = cromossomos_reparados[sobreviventes==True]
+
+                fitness_cromossomos = fitness_cromossomos[sobreviventes==True]
+                fitness_cromossomos_sorted = np.flip(np.argsort(fitness_cromossomos)[::-1])
+                cromossomos_unicos = cromossomos_unicos[fitness_cromossomos_sorted]
+                fitness_cromossomos = fitness_cromossomos[fitness_cromossomos_sorted]
+
+                if fitness_cromossomos[0] < melhor_obj:
+                    melhor_obj, melhor_sol = fitness_cromossomos[0], cromossomos_unicos[0]
+                    print(melhor_obj)
+                # print('##9')
+
+                populacao_total = cromossomos_unicos
+                
+                print(conjunto, h, problema, iter,len(populacao_total), round(calcula_diversidade(cromossomos_unicos,fitness_cromossomos),2), obj_original, fitness_cromossomos[0])
+                # print(len(populacao_total))
+                populacao_pais = cromossomos_unicos[:max(2,int(perc_pais_pop*n_pop)+1)]
+
+                fim = time.time()
+
+
+
+
+            breakpoint()
+            # for iteracoes in range(1,100):
+
+            # for cromossomo_inicial in lista_populacao_inicial:
+                
+            #     for i in range(1, 51):
+            #         populacao_total.append(mutacao(cromossomo_inicial, 0.3))
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Apresentacao 3/funcoes_GA.py
+++ Apresentacao 3/funcoes_GA.py
@@ -98,6 +98,27 @@ def _repara_solucao(set_E,set_T,ai,bi,pi,d):
     novo_set_E,novo_set_T = transforma(sol_)
     return novo_set_E,novo_set_T 
 
+
+@jit(nopython=True)
+def repara_solucao(set_E,set_T,ai,bi,pi,d):
+    sol_ = transforma_bin(set_E,set_T)
+    d_solucao = np.sum(pi[set_E])
+
+    if (d_solucao <= d): #solução não é infactível, vou sair sem fazer nada.
+        return (set_E,set_T)
+
+    z = (bi-ai)/(bi+ai)
+    violacao = d_solucao-d #o quanto eu preciso reduzir no meu d_solucao para ser factível?
+    zsort_set_E = np.flip(np.argsort(z[set_E])) #os primeiros são os maiores Zs no set_E.
+                                                #isto é, supostamente os melhores a serem movidos para o set_T.
+
+    pi_cumsum_zsort_set_E = np.cumsum(pi[set_E[zsort_set_E]]) #valor acumulado dos pi na ordem considerada a melhor.
+    mudar_ate = np.nonzero(pi_cumsum_zsort_set_E>violacao)[0][0]+1 #qual a primeira tarefa da lista ordenada que excede a violação?
+    mudar = set_E[zsort_set_E[:mudar_ate]] #colho os índices dos que precisarão ser alterados
+    sol_[mudar] = np.logical_not(sol_[mudar]) #mudo de conjunto itens necessários para reparo
+    novo_set_E,novo_set_T = transforma(sol_)
+    return novo_set_E,novo_set_T 
+
 @jit(nopython=True)
 def oprime_fracos(objs,max_pop):
     len_objs = len(objs) 
@@ -137,4 +158,86 @@ def calcula_diversidade(sols,objs):
     #substraí len_sols do numerador e denumerador para desconsiderar quando ele comparara a melhor com ela mesmo
     #mas na prática é irrelevante.
     diversidade_percent = (np.sum(igualdade))/((len(igualdade)*len_sols))
-    return diversidade_percent
+    return 1-diversidade_percent
+
+
+#@jit(nopython=True)
+def expande_populacao(sols,objs,taxa_mutacao):
+    numero_solucoes = len(sols)
+    #número de soluções com paternidade incentivada
+    n_melhores = max(1,int(numero_solucoes*0.1)) 
+    index_melhores = np.argsort(objs)[-n_melhores:]
+    #probabilidade extra reservada para os melhores serem pais.
+    incentivo_percent = 1
+    incentivo_percent_cada = incentivo_percent/n_melhores
+    #o restante é distribuído entre todas soluções.
+    ampla_disputa_percent = (1-incentivo_percent)
+    ampla_disputa_percent_cada = ampla_disputa_percent/numero_solucoes
+
+
+    #lista com a chance de cada solução ser pai:
+    #se fosse uma roleta com circunferência 1,
+    #seria o comprimento de arco de cada solução.
+    chance_ser_pai = np.array([ampla_disputa_percent_cada]*numero_solucoes)
+    chance_ser_pai[index_melhores] += incentivo_percent_cada
+
+    #numero acumulado da chance. 
+    #exemplo, se a lista de ticket é [0.3,0.9,1] 
+    #e eu sortear 0.5, o ticket sorteado foi o segundo.
+    #se eu sortear 0, o ticket sorteado foi o primeiro.
+    #se eu sortear 0.90001 foi o terceiro
+    ticket = np.cumsum(chance_ser_pai)
+
+    #número de soluções geradas é igual ao número de soluções na entrada.
+    #em outras palavras, dobra a população.
+    roleta_pai1 = np.array([np.random.random() for nao_importa in range(numero_solucoes)])
+    roleta_pai2 = np.array([np.random.random() for nao_importa in range(numero_solucoes)])
+
+    lista_pai1 = np.array([np.sum(cada_sorteio>=ticket) for cada_sorteio in roleta_pai1])
+    lista_pai2 = np.array([np.sum(cada_sorteio>=ticket) for cada_sorteio in roleta_pai2])
+
+    filhos = np.array([True]*len(sols[0]))
+    for cada_cruzamento in range(numero_solucoes):
+        parente1 = sols[lista_pai1[cada_cruzamento]]
+        parente2 = sols[lista_pai2[cada_cruzamento]]
+        filho_atual = crossover_r(parente1,parente2)
+        filhos = np.vstack((filhos,filho_atual))
+    filhos = filhos[1:]
+    
+
+    mutantes = np.array([True]*len(sols[0]))
+    for cada_filho in filhos:
+        brucutu = mutacao(cada_filho,taxa_mutacao)
+        mutantes = np.vstack((mutantes, brucutu))
+        
+    mutantes = mutantes[1:]  
+    return np.vstack((filhos,mutantes))
+
+
+@jit(nopython=True)
+def calcula_objetivo_sr(solucao, ai, bi, pi, d): #sr = sem reparo
+
+    set_E, set_T = transforma(solucao)
+    
+    if np.sum(pi[set_E])>d:
+        return 1e15
+  
+    ai_pi = ai[set_E]/pi[set_E] #apenas do set_E
+    bi_pi = bi[set_T]/pi[set_T] #apenas do set_T
+
+    ai_pi_decr = np.flip(np.argsort(ai_pi)) #ordem de ai_pi e depois por pi.
+    bi_pi_decr = np.flip(np.argsort(bi_pi))
+    set_E = set_E[ai_pi_decr]
+    set_T = set_T[bi_pi_decr]
+
+    objetivo_minimo = calcula_objetivo_minimo(ai, bi, pi, set_E, set_T, d)
+
+    return objetivo_minimo
+
+
+def avaliar_pop_sr(cromossomos, ai, bi, pi, d):
+    fitness_cromossomos = []
+    for cromossomo in cromossomos:
+        
+        fitness_cromossomos.append(calcula_objetivo_sr(cromossomo, ai, bi, pi, d))
+    return np.array(fitness_cromossomos)
